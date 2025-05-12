c53bfa8bf4e3b4e4e76d3b7558fbaaeb5d5846ff
Vijay-123-Jalla <vijaykumarrajujalla2@gmail.com>
2025-05-12 21:14:36 +0530

    Create priting1to10number.java

---
+++ priting1to10number.java
@@ -0,0 +1,17 @@
+public class recrusivetoprint10numbers {
+
+	public static void main(String[] args) {
+	printnumbers(1);	
+	}
+static int printnumbers(int n)
+{
+	if(n>10 || n==0)
+	{
+		return 0;
+	}
+	else  {
+		System.out.println(n);
+	return printnumbers(n+1);
+}
+}
+}
